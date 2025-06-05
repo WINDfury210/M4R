34bc76bf6771c39c473d58e776e76c802234ca93
WINDfury210 <mz1421@ic.ac.uk>
2025-06-05 14:52:36 +0100

    1

--- Finance/generate.py
+++ Finance/generate.py
@@ -147,7 +147,7 @@ def main():
         "channels": [32, 128, 512, 2048],
         "years": list(range(2017, 2024)),
         "samples_per_year": 100,
-        "diffusion_steps": 200,
+        "diffusion_steps": 500,
         "step_interval": 10,
         "output_dir": "generated_sequences"
     }
