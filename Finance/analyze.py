9485ba829cd5c6c71acdf1a29f0908b403162e2b
WINDfury210 <mz1421@ic.ac.uk>
2025-06-05 10:44:24 +0100

    1

--- Finance/analyze.py
+++ Finance/analyze.py
@@ -447,8 +447,8 @@ def validate_generated_data(config):
 
 if __name__ == "__main__":
     config = {
-        "generated_dir": "generated_sequences/generation_20250605_032900",
-        "output_dir": "validation_results/generated_20250605_032900",
+        "generated_dir": "generated_sequences/generation_20250605_022420",
+        "output_dir": "validation_results/generated_20250605_022420",
         "data_path": "financial_data/sequences/sequences_256.pt",
         "real_metrics_dir": "real_metrics",
         "years": list(range(2017, 2024))
